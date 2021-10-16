ab619640d778eb49dc6084bcb5b2d517daf66969
sa-atsushi <takeuma26man@gmail.com>
2021-10-16 07:49:51 +0000

    commit all

---
+++ script.js
@@ -0,0 +1,45 @@
+/*
+
+let omikujiButton = document.getElementById("omikuji-button");
+let result = document.getElementById("result");
+
+function omikuji() {
+  let r = Math.random();
+  if (r < 0.2) {
+    result.textContent = "大吉";
+    result.style.color = "red";
+  } else if (r < 0.7) {
+    result.textContent = "吉";
+    result.style.color = "black";
+  } else {
+    result.textContent = "凶";
+    result.style.color = "blue";
+  }
+}
+
+omikujiButton.onclick = omikuji;
+
+*/
+
+let startButton = document.getElementById("start-button");
+let stopButton = document.getElementById("stop-button");
+let result = document.getElementById("result");
+let timerId;
+
+function start() {
+  timerId = setInterval(timer, 1000);
+}
+function stop() {
+  clearInterval(timerId);
+}
+let time = 0;
+function tick() {
+  time = time + 1;
+}
+function timer() {
+  tick();
+  result.textContent = time;
+}
+
+startButton.onclick = start;
+stopButton.onclick = stop;
